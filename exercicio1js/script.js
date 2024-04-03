826919f719a3495b19797f0ce09c2bde99fee10b
victoriacolonna <148501617+victoriacolonna@users.noreply.github.com>
2024-04-03 12:33:09 -0300

    Add files via upload

---
+++ exercicio1js/script.js
@@ -0,0 +1,11 @@
+let num1 = 5
+let num2 = 3 
+let num3 = 8 
+
+let peso1 = 4
+let peso2 = 2
+let peso3 = 6
+
+let media = (num1 * peso1 + num2 * peso2 + num3 * peso3) / (peso1 + peso2 + peso3)
+
+console.log("A média é: ", media)
